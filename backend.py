952f366120e4ec52049fb01afc7459b1c54bbb69
Gerard <dothanhvinh1@gmail.com>
2023-07-16 12:42:58 +0700

    connect backend with frontend

--- backend.py
+++ backend.py
@@ -10,7 +10,7 @@ class Chatbot:
         openai.api_key = API_KEY
 
     def get_response(self, user_input):
-        response = openai.Completion.create(engine="text-davinci-003", prompt=user_input, max_tokens=3000, temperature=0.5).choices[0].text
+        response = openai.Completion.create(engine="text-davinci-003", prompt=user_input, max_tokens=500, temperature=0.5).choices[0].text
         return response
     
 
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from PyQt6.QtWidgets import QMainWindow, QTextEdit, QLineEdit, QPushButton, QApplication
 import sys
+from backend import Chatbot
+import threading
 
 
 
 class ChatbotWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.chatbot = Chatbot()
 
         self.setMinimumSize(700, 500)
 
@@ -15,12 +18,28 @@ class ChatbotWindow(QMainWindow):
 
         self.input_field = QLineEdit(self)
         self.input_field.setGeometry(10 , 340, 480, 40)
+        self.input_field.returnPressed.connect(self.send_message)
 
         self.button = QPushButton("Send", self)
         self.button.setGeometry(500, 340, 100, 40)
+        self.button.clicked.connect(self.send_message)
 
         self.show()
 
+    def send_message(self):
+        user_input = self.input_field.text().strip()
+        self.chat_area.append(f"<p style='color:#333333'> Me: {user_input} </p>")
+        self.input_field.clear()
+        
+        # thread = threading.Thread(target=self.get_bot_response, args=(user_input, ))
+        # thread.start()
+        response = self.chatbot.get_response(user_input)
+        self.chat_area.append(f"<p style='color:#333333; background-color: #E9E9E9'>Bot: {response} </p>")
+        
+    
+    # def get_bot_response(self, user_input):
+    #     response = self.chatbot.get_response(user_input)
+    #     self.chat_area.append(f"<p style='color:#333333; background-color: #E9E9E9'> OpenAi: {response} </p>")
 
 app = QApplication(sys.argv)
 main_window = ChatbotWindow()
