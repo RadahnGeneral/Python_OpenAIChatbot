f93f223e5fc28e73a5545a35f041694bee0cefa9
Gerard <dothanhvinh1@gmail.com>
2023-07-16 11:59:36 +0700

    fix error in api key

--- backend.py
+++ backend.py
@@ -3,11 +3,11 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-password_env = os.getenv("OPEN_API_KEY")
+API_KEY = os.getenv("OPENAI_API_KEY")
 
 class Chatbot:
     def __init__(self):
-        openai.api_key = "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"
+        openai.api_key = API_KEY
 
     def get_response(self, user_input):
         response = openai.Completion.create(engine="text-davinci-003", prompt=user_input, max_tokens=3000, temperature=0.5).choices[0].text
@@ -15,6 +15,7 @@ class Chatbot:
     
 
 if __name__ == "__main__":
+   
     chatbot = Chatbot()
     response = chatbot.get_response("what do you think about Elon Musk?")
     print(response)
